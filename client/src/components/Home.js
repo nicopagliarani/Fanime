f5e2b0d1dc378bf4777fa6b0577f6c036e158a38
Adryan Cadet <adryan-pro@hotmail.com>
2022-04-07 23:47:17 +0200

    secure Route for Home and Profile done

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,21 @@
 import axios from "axios";
 import { API_BASE_URL } from "../consts";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthProviderWrapper";
 
 export function Home() {
   const [anime, setAnime] = useState([]);
   const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    console.log(user);
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -13,14 +23,13 @@ export function Home() {
         const data = response.data;
         setAnime(data.animesData);
       } catch (err) {
+        console.log("We got an error");
+        console.error(err);
         console.log(err.response.data);
-        if (err.response.status === 401) {
-          navigate("/login");
-        }
       }
     };
     fetchData();
-  }, [navigate]);
+  }, []);
   return (
     <>
       {anime.map((element) => {
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,18 +1,21 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import axios from "axios";
 import { API_BASE_URL } from "../consts";
 import { UserNameAndPasswordForm } from "./UserNameAndPasswordForm";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthProviderWrapper";
 
 export function Login() {
   const [errorState, SetErrorState] = useState();
   const navigate = useNavigate();
+  const { addUserToContext } = useContext(AuthContext);
   const login = async (formState) => {
     try {
       console.log("formState", formState);
       console.log(API_BASE_URL);
       const response = await axios.post(`${API_BASE_URL}/api/login`, formState);
       console.log(response.data);
+      addUserToContext(response.data.user);
       navigate("/profile");
     } catch (err) {
       console.log(err.response, "error");
