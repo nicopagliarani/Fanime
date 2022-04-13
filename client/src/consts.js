b309ba78a0b95d4874b8c5de76491651a3da70ad
Adryan Cadet <adryan-pro@hotmail.com>
2022-04-13 18:01:37 +0200

    FIX

--- client/src/consts.js
+++ client/src/consts.js
@@ -3,6 +3,6 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL === "production"
+  process.env.NODE_ENV === "production"
     ? "https://fanime-app.herokuapp.com"
     : "http://localhost:5005";
